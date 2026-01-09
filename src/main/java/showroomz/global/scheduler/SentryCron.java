237c4f17b2f73b00f7753816528cc17fb6e2acf8
hc784 <kkjj1211@naver.com>
2026-01-10 01:38:45 +0900

    fix: sentry jakrta로 변경

--- src/main/java/showroomz/global/scheduler/SentryCron.java
+++ src/main/java/showroomz/global/scheduler/SentryCron.java
@@ -2,12 +2,15 @@ package showroomz.global.scheduler;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import io.sentry.spring.jakarta.checkin.SentryCheckIn;
+
+
 @Component
 public class SentryCron {
 
   @Scheduled(fixedRate = 60 * 1000L)
-  @io.sentry.spring.checkin.SentryCheckIn("328698e0a0cd")
-  void execute() throws InterruptedException {
+  @SentryCheckIn("328698e0a0cd")
+  public void execute() throws InterruptedException {
     // your task code
   }
 }
