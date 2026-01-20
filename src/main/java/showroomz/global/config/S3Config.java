1b87348e586f4ed855e8e304464ccaab9e3314b0
hc784 <kkjj1211@naver.com>
2026-01-20 13:13:23 +0900

    feat: 앱 내부에서 aceess key 대신 oidc로 변경

--- src/main/java/showroomz/global/config/S3Config.java
+++ src/main/java/showroomz/global/config/S3Config.java
@@ -1,12 +1,10 @@
 package showroomz.global.config;
 
 import lombok.RequiredArgsConstructor;
-import showroomz.global.config.properties.S3Properties;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import showroomz.global.config.properties.S3Properties;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
@@ -18,14 +16,11 @@ public class S3Config {
 
     @Bean
     public S3Client s3Client() {
-        AwsBasicCredentials awsCredentials = AwsBasicCredentials.create(
-                s3Properties.getAccessKey(),
-                s3Properties.getSecretKey()
-        );
-
+        // [변경] AccessKey, SecretKey를 직접 주입받지 않고,
+        // 실행 환경(EC2 IAM Role, 환경변수 등)에서 자동으로 자격 증명을 가져오는 DefaultCredentialsProvider 사용
         return S3Client.builder()
                 .region(Region.of(s3Properties.getRegion()))
-                .credentialsProvider(StaticCredentialsProvider.create(awsCredentials))
+                .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
     }
 }
