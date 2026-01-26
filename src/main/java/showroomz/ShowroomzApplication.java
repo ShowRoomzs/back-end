b8cd784f8cfe2d794c4f62d096d8d5b044950311
hc784 <kkjj1211@naver.com>
2026-01-26 13:54:25 +0900

    config: flyway 설정 로컬 배포 분리

--- src/main/java/showroomz/ShowroomzApplication.java
+++ src/main/java/showroomz/ShowroomzApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
@@ -19,7 +18,6 @@ import org.springframework.web.HttpRequestMethodNotSupportedException;
 @SpringBootApplication
 @EnableScheduling
 @EnableAsync
-@EnableJpaAuditing
 @EnableConfigurationProperties({AppProperties.class, CorsProperties.class, S3Properties.class})
 public class ShowroomzApplication {
 	public static void main(String[] args) {
