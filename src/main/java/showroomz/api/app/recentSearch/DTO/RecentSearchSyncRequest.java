228d80b81c3bf774c567d54274863441911f808e
hc784 <kkjj1211@naver.com>
2026-02-05 18:21:43 +0900

    feat: 최근 검색어 동기화 createdAt 추가

--- src/main/java/showroomz/api/app/recentSearch/DTO/RecentSearchSyncRequest.java
+++ src/main/java/showroomz/api/app/recentSearch/DTO/RecentSearchSyncRequest.java
@@ -4,11 +4,21 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.Instant;
 import java.util.List;
 
 @Getter
 @Setter
 @NoArgsConstructor
 public class RecentSearchSyncRequest {
-    private List<String> keywords;
+
+    private List<RecentSearchSyncItem> keywords;
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    public static class RecentSearchSyncItem {
+        private String keyword;
+        private Instant createdAt;
+    }
 }
